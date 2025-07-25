08e5a689dec02074fedd0358bcb877475bcb0e92
Marco Romeny <marco@mimecom.net>
2025-07-25 14:38:43 +0200

    Prep for pdf deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://book.gh.kioskkiosk.com",
-  //base: '',
+  // base: ".",
   build: {
     // Example: Generate `page.html` instead of `page/index.html` during build.
     // format: "file",
@@ -18,9 +18,26 @@ export default defineConfig({
   server: {
     port: 9000,
   },
-  integrations: [mdx(), sitemap(), tailwind({nesting:true})],
-  vite:{
+  integrations: [mdx(), sitemap(), tailwind({ nesting: true })],
+  vite: {
+    // base:'./',
     build: {
       minify: false,
-  }}
+    },
+    // plugins: [
+    //   {
+    //     name: 'remove-leading-slash',
+    //     transform(code, id) {
+    //       if (id.endsWith('.astro') || id.endsWith('.html') || id.endsWith('.md') || id.endsWith('.mdx')) {
+    //         return {
+    //           code: code
+    //             .replace(/(href|src)=(["'])\//g, '$1=$2')
+    //             .replace(/(href|src)=\{(["'])\//g, '$1={$2'),
+    //           map: null
+    //         };
+    //       }
+    //     }
+    //   }
+    // ]
+  },
 });
